17ce933b722dd1bd60f1166ff46e3cfa5802583a
DarraghMcKernan <dmckernan77@gmail.com>
2025-05-12 19:46:07 +0100

    fix

--- Recycling Rats/Assets/Scripts/BuildSceneClientUI.cs
+++ Recycling Rats/Assets/Scripts/BuildSceneClientUI.cs
@@ -1,35 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Unity.Netcode;
+using System.Collections;
 
 public class BuildSceneClientUI : MonoBehaviour
 {
     public Button readyButton;
 
-    void Start()
+    private void Start()
     {
-        if (readyButton != null)
-        {
-            readyButton.onClick.AddListener(OnClientReady);
-        }
+        readyButton.interactable = false;
+        StartCoroutine(EnableWhenPlayerObjectReady());
+    }
+
+    IEnumerator EnableWhenPlayerObjectReady()
+    {
+        // Wait until Netcode assigns the PlayerObject
+        yield return new WaitUntil(() =>
+            NetworkManager.Singleton != null &&
+            NetworkManager.Singleton.IsClient &&
+            NetworkManager.Singleton.LocalClient != null &&
+            NetworkManager.Singleton.LocalClient.PlayerObject != null
+        );
+
+        readyButton.interactable = true;
+        readyButton.onClick.AddListener(OnClientReady);
     }
 
     void OnClientReady()
     {
-        if (NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsHost)
+        var player = NetworkManager.Singleton.LocalClient.PlayerObject;
+        if (player != null)
         {
-            var player = NetworkManager.Singleton.LocalClient.PlayerObject;
-            if (player != null)
-            {
-                player.GetComponent<ClientReadyCommunicator>().SendReadyServerRpc();
-                readyButton.interactable = false;
-            }
-            else
-            {
-                Debug.LogError("PlayerObject is null on client.");
-            }
+            player.GetComponent<ClientReadyCommunicator>().SendReadyServerRpc();
+            readyButton.interactable = false;
+        }
+        else
+        {
+            Debug.LogError("PlayerObject is still null on client.");
         }
     }
 }
