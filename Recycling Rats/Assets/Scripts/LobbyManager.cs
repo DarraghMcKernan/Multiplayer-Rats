4eea0b36cb779091beec3bec4a76d7615ab09a0e
DarraghMcKernan <dmckernan77@gmail.com>
2025-05-12 17:24:41 +0100

    lobby code visible

--- Recycling Rats/Assets/Scripts/LobbyManager.cs
+++ Recycling Rats/Assets/Scripts/LobbyManager.cs
@@ -13,6 +13,7 @@ public class LobbyUI : MonoBehaviour
     public Image player1Indicator;
     public Image player2Indicator;
     public TMP_InputField joinCodeInput;
+    public TMP_Text lobbyCode;
 
     private void Start()
     {
@@ -32,7 +33,7 @@ public class LobbyUI : MonoBehaviour
         var task = RelayManager.Instance.CreateRelay();
         yield return new WaitUntil(() => task.IsCompleted);
         string joinCode = task.Result;
-        Debug.Log("Share this join code with your friend: " + joinCode);
+        lobbyCode.text = "Lobby Code: " + joinCode;
     }
 
     void StartRelayClient()
@@ -43,14 +44,14 @@ public class LobbyUI : MonoBehaviour
 
     void OnClientConnected(ulong clientId)
     {
-        if (NetworkManager.Singleton.ConnectedClientsList.Count >= 1)
-            player1Indicator.color = Color.green;
+        player1Indicator.color = Color.green;
+        player2Indicator.color = Color.green;
 
-        if (NetworkManager.Singleton.ConnectedClientsList.Count >= 2)
-            player2Indicator.color = Color.green;
-
-        if (NetworkManager.Singleton.IsHost && NetworkManager.Singleton.ConnectedClientsList.Count == 2)
+        if (NetworkManager.Singleton.IsHost &&
+            NetworkManager.Singleton.ConnectedClientsList.Count == 2)
+        {
             startGameButton.gameObject.SetActive(true);
+        }
     }
 
     void StartGame()
