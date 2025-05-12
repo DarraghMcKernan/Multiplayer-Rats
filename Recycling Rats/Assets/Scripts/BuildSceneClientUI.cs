ad3e69ba6a9d86a64590611e61e14e0ad27c3ea4
JackNulty <93530525+JackNulty@users.noreply.github.com>
2025-05-12 19:39:36 +0100

    penis

--- Recycling Rats/Assets/Scripts/BuildSceneClientUI.cs
+++ Recycling Rats/Assets/Scripts/BuildSceneClientUI.cs
@@ -14,26 +14,22 @@ public class BuildSceneClientUI : MonoBehaviour
         {
             readyButton.onClick.AddListener(OnClientReady);
         }
-        else
-        {
-            Debug.LogError("ReadyButton not assigned!");
-        }
     }
 
     void OnClientReady()
     {
-        if (!NetworkManager.Singleton.IsClient || NetworkManager.Singleton.IsHost)
-            return;
-
-        if (BuildSceneNetworkState.Instance != null)
+        if (NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsHost)
         {
-            BuildSceneNetworkState.Instance.SetClientReadyServerRpc(true);
-            readyButton.interactable = false;
-            Debug.Log("Client marked ready.");
-        }
-        else
-        {
-            Debug.LogWarning("BuildSceneNetworkState.Instance is null. This object only exists on the host.");
+            var player = NetworkManager.Singleton.LocalClient.PlayerObject;
+            if (player != null)
+            {
+                player.GetComponent<ClientReadyCommunicator>().SendReadyServerRpc();
+                readyButton.interactable = false;
+            }
+            else
+            {
+                Debug.LogError("PlayerObject is null on client.");
+            }
         }
     }
 }
